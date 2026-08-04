a7f3b24298c89c1204a79b621f19b35a1ae0e2f2
SUJAL KUMAR <163004854+coco2534@users.noreply.github.com>
2026-08-04 16:12:02 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        
+        int chars[26] = {0};
+
+        for(int i = 0; i<s.size(); i++){
+            chars[s[i] - 'a']++;
+        }
+
+        for(int i = 0; i<t.size(); i++){
+            chars[t[i] - 'a']--;
+        }
+
+        for(int i = 0; i<26; i++){
+            if(chars[i] != 0) return false;
+        }
+
+        return true;
+    }
+};
